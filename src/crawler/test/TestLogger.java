8659c6a953b82202e820d24760d3f7b9cf0288e8
Chi Yang <alitzlan@gmail.com>
2016-04-08 10:00:03 -0400

    add test file for logger

---
+++ src/crawler/test/TestLogger.java
@@ -0,0 +1,11 @@
+package crawler.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogger {
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(TestLogger.class);
+		logger.info("Hello World");
+	}
+}
